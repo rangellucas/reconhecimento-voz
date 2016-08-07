83723d8e0813c5bfb84eccb43b31b261603eabed
Your Name <you@example.com>
2016-08-06 21:57:07 -0300

    Teste voz

---
+++ speech-color-changer/script.js
@@ -0,0 +1,66 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var grammar = '#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;'
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+//recognition.continuous = false;
+recognition.lang = 'pt-BR';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+
+var diagnostic = document.querySelector('.output');
+var bg = document.querySelector('html');
+
+document.body.onclick = function() {
+  recognition.start();
+  console.log('Ready to receive a color command.');
+}
+
+recognition.onresult = function(event) {
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The first [0] returns the SpeechRecognitionResult at position 0.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The second [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
+  var txt = event.results[0][0].transcript;
+
+  diagnostic.textContent = 'Result received: ' + txt + '.';
+  //bg.style.backgroundColor = color;
+  console.log(txt.indexOf("voltar"));
+  txt = txt.toLowerCase();
+
+  var msg = new SpeechSynthesisUtterance('Que boca suja Rodrigo!');
+  msg.lang = 'pt-BR';
+  window.speechSynthesis.speak(msg);
+
+  if(txt.indexOf("ir para contato")>=0) {
+    window.location.href = "contato.html";
+    //console.log(txt.indexOf("voltar"));
+  }else if(txt.indexOf("voltar para")>=0){
+    window.history.go(-1);
+  }
+
+  console.log('Confidence: ' + event.results[0][0].confidence);
+  console.log('Result: ' + event.results[0][0].transcript);
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+  diagnostic.textContent = 'I didnt recognise that color.';
+}
+
+recognition.onerror = function(event) {
+  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
